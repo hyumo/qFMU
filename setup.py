f38bfea26f5b8ce2e5419337fe4c9c3500e8faea
hyumo <yuhang.neu@gmail.com>
2021-10-23 10:22:38 -0400

    update

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hyumo/qfmu',
-    version='0.3.1',
+    version='0.1.0',
     zip_safe=False,
 )
 
